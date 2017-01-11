4a7352a84b54041b61733b7a29d945d2d84e0576
Osman Idris <osmanidris@Osmans-MacBook-Pro.local>
2017-01-11 16:57:56 -0500

    inital commit

---
+++ src/MyUniqueStrings.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by osmanidris on 1/11/17.
+ */
+public class MyUniqueStrings {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        String myStrings[] = new String[5];
+        boolean flag=true;
+        System.out.println("Unique Strings ....");
+        for (int i=0; i<5; i++){
+            System.out.println("Enter the String No "+i+":");
+            myStrings[i] = scanner.next();
+            for(int j= 0 ;j <= i; j++){
+                if(myStrings[j].equals(myStrings[i])){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag){
+            System.out.println("Your strings entered are unique");
+        }else{
+            System.out.println("You have entered duplicate strings");
+        }
+
+
+    }
+}
